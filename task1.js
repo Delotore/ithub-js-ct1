e040b0c53fa6a3a5581ced5a1461427bdc7d2dfe
Roman Ashurov <roman.ash.dm@gmail.com>
2025-09-11 13:56:39 +0300

    ashct1

---
+++ task1.js
@@ -0,0 +1,29 @@
+/**Задача №1
+
+В программе объявлены две переменные — price и range. Переменная range может принимать одно из трех строковых значений — month/day/week. Переменная price хранит в себе числовое значение. Необходимо написать программу, которая формирует строку с использованием данных переменных по следующему шаблону: 
+
+<значение price> Р в <эквивалент значения range на русском языке> 
+
+Пример значений переменных: 
+
+let price = 10000 
+
+let range = "day" 
+
+Пример результата:
+
+10000 Р в день */
+
+const price = Number(prompt('íncome'))
+const range = prompt('period of time(month,day,week)')
+
+const formattedPrice = price.toLocaleString('ru', {
+    style: "currency",
+    currency: "RUB"
+})
+const result =`${formattedPrice} in a ${range}`
+console.log(result)
+
+
+
+
